465a4f5a794a2dc165663309c82fa62e2547b3d7
dyanez11 <diegoayanezvega@hotmail.com>
2020-10-28 18:37:22 -0600

    feat: add instancies funcionality

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -39,6 +39,24 @@ export class ButtonComponent implements OnInit {
       return;
     } else {
       const numbersArray = this.number.split("");
+      const oneInstancies = numbersArray.filter((number) => number === "1");
+      const twoInstancies = numbersArray.filter((number) => number === "2");
+      const threeInstancies = numbersArray.filter((number) => number === "3");
+      const fourInstancies = numbersArray.filter((number) => number === "4");
+      const fiveInstancies = numbersArray.filter((number) => number === "5");
+      const sixInstancies = numbersArray.filter((number) => number === "6");
+      const sevenInstancies = numbersArray.filter((number) => number === "7");
+      const eightInstancies = numbersArray.filter((number) => number === "8");
+      const nineInstancies = numbersArray.filter((number) => number === "9");
+      console.log('The 1 was repeated ', oneInstancies.length, ' times');
+      console.log('The 2 was repeated ', twoInstancies.length, ' times');
+      console.log('The 3 was repeated ', threeInstancies.length, ' times');
+      console.log('The 4 was repeated ', fourInstancies.length, ' times');
+      console.log('The 5 was repeated ', fiveInstancies.length, ' times');
+      console.log('The 6 was repeated ', sixInstancies.length, ' times');
+      console.log('The 7 was repeated ', sevenInstancies.length, ' times');
+      console.log('The 8 was repeated ', eightInstancies.length, ' times');
+      console.log('The 9 was repeated ', nineInstancies.length, ' times');
       let numbersRepeated = [];
       let lastNumber = 0;
       for (let i = 0; i < numbersArray.length; i++) {
